294150ef4afcc63e5cde4738114664b47cbc1055
cowboy8625 <cowboy8625@yahoo.com>
2024-09-22 20:08:59 -0500

    fix test

--- src/symbol_table/test.rs
+++ src/symbol_table/test.rs
@@ -4,6 +4,7 @@ use super::*;
 use crate::parser::parser;
 use chumsky::prelude::Parser;
 use pretty_assertions::assert_eq;
+use std::collections::HashMap;
 
 #[test]
 fn test_table_creation() {
@@ -14,5 +15,58 @@ fn test_table_creation() {
     let ast = parser().parse(src).unwrap();
     let symbol_table = SymbolWalker::default().walk(&ast).unwrap();
     eprintln!("{:#?}", symbol_table);
-    assert!(false);
+    assert_eq!(
+        symbol_table.lookup("add"),
+        Some(&Symbol {
+            id: 0,
+            name: "add".to_string(),
+            symbol_type: SymbolType::Dynamic,
+            kind: SymbolKind::Variable,
+            scope: Scope::Global,
+            scope_level: 0,
+            location: None,
+        })
+    );
+    assert_eq!(
+        symbol_table.lookup("main"),
+        Some(&Symbol {
+            id: 0,
+            name: "main".to_string(),
+            symbol_type: SymbolType::Function(vec![], Box::new(SymbolType::Dynamic)),
+            kind: SymbolKind::Function,
+            scope: Scope::Global,
+            scope_level: 1,
+            location: None,
+        })
+    );
+    assert_eq!(
+        symbol_table.lookup("lambda_0"),
+        Some(&Symbol {
+            id: 0,
+            name: "lambda_0".to_string(),
+            symbol_type: SymbolType::Function(
+                vec![SymbolType::Dynamic],
+                Box::new(SymbolType::Dynamic)
+            ),
+            kind: SymbolKind::Lambda,
+            scope: Scope::Global,
+            scope_level: 1,
+            location: None,
+        })
+    );
+    assert_eq!(
+        symbol_table.lookup("lambda_1"),
+        Some(&Symbol {
+            id: 1,
+            name: "lambda_1".to_string(),
+            symbol_type: SymbolType::Function(
+                vec![SymbolType::Dynamic],
+                Box::new(SymbolType::Dynamic)
+            ),
+            kind: SymbolKind::Lambda,
+            scope: Scope::Function,
+            scope_level: 2,
+            location: None,
+        })
+    );
 }
