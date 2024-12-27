9d4ff44233471264d71f3ed6d7e4e03d5123fc7d
cowboy8625 <cowboy8625@yahoo.com>
2024-12-27 14:48:48 -0600

    span

--- benches/machine_function_call.rs
+++ benches/machine_function_call.rs
@@ -8,12 +8,14 @@ pub fn benchmark_call(c: &mut Criterion) {
         Instruction::Push(Box::new(Value::F64(1.0))), // 0x00   0   02 04 00 00 00 00 00 00 F0 3F
         Instruction::Push(Box::new(Value::F64(1.0))), // 0x0A  10   02 04 00 00 00 00 00 00 F0 3F
         Instruction::Jump(38),                        // 0x14  20   1C 26 00 00 00
-        Instruction::GetLocal(RuntimeMetadata::new(0, "x")), // 0x19  25   16 00 00 00 00
-        Instruction::GetLocal(RuntimeMetadata::new(1, "y")), // 0x1E  30   16 01 00 00 00
+        Instruction::GetLocal(RuntimeMetadata::new(0, "x", 0..1)), // 0x19  25   16 00 00 00 00
+        Instruction::GetLocal(RuntimeMetadata::new(1, "y", 0..1)), // 0x1E  30   16 01 00 00 00
         Instruction::Add(2),                          // 0x23  35   03 02
         Instruction::Return,                          // 0x25  37   01
         Instruction::Push(Box::new(Value::Callable(Box::new(Callable::new(
-            25, "apply",
+            25,
+            "apply",
+            0..1,
         ))))), // 0x26  38   02 08 19 00 00 00
         Instruction::Call(2),                         // 0x2C  44   11 02
     ]
